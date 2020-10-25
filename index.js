e40088da03ad9fbe7a82b8943ded0e6e0a0a614c
Kevin.K <kevinkusky@gmail.com>
2020-10-25 12:32:55 -0400

    adds skeleton for form event handler

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// let element = document.querySelector('#idName');
-// // element.remove();
-// element.classList.add('className');
-
 // let container = document.querySelector('.container');
 // container.prepend(element);
 
@@ -11,10 +7,6 @@
 
 // let items = document.querySelectorAll('.allElementsWithItemName');
 
-// element.addEventListener('click', function(){
-//     console.log('you clicked');
-// });
-
 let animationParams = {};
 
 const oceanBreathForm = () => {
@@ -22,14 +14,24 @@ const oceanBreathForm = () => {
     // inhale, pause, exhale
     let descriptorPar = document.querySelector('.breath-descriptor');
     descriptorPar.innerHTML = `The Ujjayi breathing technique might be familiar if you have practiced yoga before.
-    Ujjayi breath is sometimes refered to as a breath of victory or an oceanic breath.  This breath is an audible breath
-    with some constriction from the back of the throat.  A great cue is to think of your inhales and exhales
-    as an attempt to fog up a mirror.  To begin your practice, please fill out the form for inhale and exhale times.
+    Ujjayi breath is sometimes refered to as a breath of victory or an oceanic breath.  The inhale is an audible breath
+    with some constriction from the back of the throat.  Your exhales can be thought of as trying to fog up a mirror.  
+    To begin your practice, please fill out the form for inhale and exhale times. 
     If you wish, I have also included a pause time if you wish to add an apnea to your practice.`;
+
+    // let breathParamsField = document.querySelector('.breath-specific-options');
+
+    // insertAdjacentHTML
 
     let oceanFormSubmit = document.querySelector('.inactive-submit');
     oceanFormSubmit.classList.add('show-submit');
 
+
+    let oceanForm = document.querySelector('.app-director-form');
+    oceanForm.addEventListener('submit', e => {
+        e.preventDefault();
+        console.log('Ujjayi Submit');
+    });
     console.log('this will contain wet html');
 };
 
@@ -41,12 +43,19 @@ const fireBreathForm = () => {
 
     descriptorPar.innerHTML = `Kapalabhati, or skull shining breath, is a 
         yogic breathing technique aimed to clear the mind, energize the body, and stimulate digestion.
-        To practice this breath, first we will start with a long inhale and exhale to prepare for the practice.
-        Next, will be a deep inhale to set up the breath which should have the following intentions:
-        The exhalation is a forceful contracting of the abdoment, while the inhale is more passive and as
-        an automatic response to the exhale.  While the cues should assist, the intention should be to follow a
-        steady and rigorous rythm that is not strenuous to maintain.  To start your practice, you only need to press
-        the Start button and enjoy your practice.`;
+        To practice this breath, first start with a long inhale and exhale to prepare for the practice.
+        Next, take a deep inhale to set up the breath which should have the following intentions:
+        The exhalation is a forceful contracting of the abdomen, while the inhale is more of a passive and automatic response to the exhale.
+        While the cues should assist, the intention should be to follow a steady and rigorous rythm that is not strenuous 
+        to maintain.  To start your practice, you only need to press the Start button and enjoy your practice.`;
+
+    // let breathParamsField = document.querySelector('.breath-specific-options');
+
+    let fireForm = document.querySelector('.app-director-form');
+    fireForm.addEventListener('submit', e => {
+        e.preventDefault();
+        console.log('Kapalabhati Submit');
+    });
 
     let fireFormSubmit = document.querySelector('.inactive-submit');
     fireFormSubmit.classList.add('show-submit');
@@ -70,6 +79,13 @@ const altBreathForm = () => {
         Each inhale is to be followed with an exhale through the opposite nostral.  
         To start your practice, please fill our the form for inhale and exhale times.  Also, there is an optional pause time
         which will add an apnea after each inhalation.`;
+
+    // let breathParamsField = document.querySelector('.breath-specific-options');
+    let altForm = document.querySelector('.app-director-form');
+    altForm.addEventListener('submit', e => {
+        e.preventDefault();
+        console.log('Nadi Submit');
+    });
 
     let altFormSubmit = document.querySelector('.inactive-submit');
     altFormSubmit.classList.add('show-submit');
@@ -104,4 +120,3 @@ const radioFormDirector = () => {
 document.addEventListener("DOMContentLoaded", () => {
     radioFormDirector();
 });
-
