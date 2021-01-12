ce27652f31a78b838922034c7e114fd1584f8d77
kevinkusky <kevinkusky@gmail.com>
2021-01-11 19:48:34 -0500

    cleans up description text

--- src/forms/alt.js
+++ src/forms/alt.js
@@ -3,19 +3,31 @@ const altBreathForm = () => {
     // modal descriptor and append for animation details
     let descriptorPar = document.querySelector(".selected-breath-description");
     descriptorPar.innerHTML = `
-            <p class='breath-descriptor'>
-                Nadi Shodhana is known as Alternate Nostril Breathing.  As the name might suggest,
-                this breath will involve using the hands as a way of restricting the breath in and out of one nostral at a time.
-                This type of breathing can help balance the hemispheres of the brain as well as help focus the mind and purify
-                the central nervous system. This is a great breath to practice before a meditation practice. As a suggestion,
-                begin your practice with an inhale from the left nostral with your right thumb blocking the right nasal passage.
-                Each inhale is to be followed with an exhale through the opposite nostral. To start your practice, please fill out 
-                the form for inhale and exhale times.  Also, there is an optional pause time which will add an apnea after each inhalation.
+            <span class='breath-descriptor'>
+                Nadi Shodhana is known as Alternate Nostril Breathing.
                 <br>
-                At any time you may press the SPACE BAR key to pause the display and access a menu.
+                <br>
+                As the name might imply, this breath will involve using the hands 
+                as a way of restricting the breath in and out of one nostral at a time.
+                <br>
+                <br>
+                This type of breathing can help balance the hemispheres of the brain as well as help focus the mind and purify
+                the central nervous system.
+                <br>
+                This is a great breath to practice before a meditation practice. 
+                To follow along with the animation to practice this breath follow these instructions:
                 <br>
                 <a target='_blank' href='https://youtu.be/l11qFpRqhIQ?t=122'>Follow video for a more in depth instruction on this breath</a>
-            </p>
+                <ul>
+                    <li>Utilize your right thumb and ring finger to alternate blocking your left and right nasal passages</li>
+                    <li>Begin your practice with your thumb blocking your right nostral - inhaling into the left nasal passage</li>
+                    <li>On the exhale, block your left nostral with your ring finger and release the right nasal passage - exhaling out</li>
+                    <li>If you added a pause or apnea to your practice, you may wish to hold or release both nasal passages</li>
+                </ul>
+                To start your practice, please fill out the form for inhale and exhale times.  Also, there is an optional pause time which will add an apnea after each part of the breath.
+                <br>
+                At any time you may press the SPACE BAR key to pause the display and access a menu or press the R key to navigate back to the home form.
+            </span>
         `;
 
     // Inserts inputs and adds event listeners
--- src/forms/fire.js
+++ src/forms/fire.js
@@ -2,19 +2,32 @@
 const fireBreathForm = () => {
     let descriptorPar = document.querySelector(".selected-breath-description");
     descriptorPar.innerHTML = `
-            <p class='breath-descriptor'>
-                Kapalabhati, or skull shining breath, is a yogic breathing technique aimed to clear the mind, 
-                energize the body, and stimulate digestion.  To practice this breath, 
-                first start with a long inhale and exhale to prepare for the practice.
-                Next, take a deep inhale to set up the breath which should have the following intentions:
-                The exhalation is a forceful contracting of the abdomen, while the inhale is more of a passive and automatic response to the exhale.
-                While the cues should assist, the intention should be to follow a steady and rigorous rythm that is not strenuous 
-                to maintain.  To start your practice, you only need to press the Start button and enjoy your practice.  
+            <span class='breath-descriptor'>
+                Kapalabhati breath is also known as skull shining breath
                 <br>
-                At any time you may press the SPACE BAR key to pause the display and access a menu.
+                <br>
+                Kapalabhati is a yogic breathing technique aimed to clear the mind, 
+                energize the body, and stimulate digestion.
+                <br>
+                <br>
+                To practice this breath, Follow these instructions with the animation:
                 <br>
                 <a target='_blank' href='https://youtu.be/y364TyKk5Fg?t=131'>Follow video for more in debth breath description</a>
-            </p>
+                <ul>
+                    <li>Start with a long inhale and exhale to prepare for the practice.</li>
+                    <li>Take one more deep inhale to set up the breath</li>
+                    <li>Begin the Kapalabhati which should have the following intentions:
+                        <ul>
+                            <li>Exhalations have a forceful contraction of the abdomen</li>
+                            <li>Inhalations are more passive - like an automatic response to the exhale</li>
+                            <li>Together, your breaths should follow a steady and rigorous rythem that is not stenuous to maintain</li>
+                        </ul>
+                    </li>
+                </ul>
+                To start your practice, you only need to press the Start button and enjoy your practice.  
+                <br>
+                At any time you may press the SPACE BAR key to pause the display and access a menu or press the R key to navigate back to the home form.
+            </span>
         `;
 
     let breathParamsField = document.querySelector(".breath-specific-options");
